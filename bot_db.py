013e889e7c6f619eb525cac66610ebfab94db75a
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2019-12-30 22:49:36 +0200

    о

--- bot_db.py
+++ bot_db.py
@@ -9,7 +9,7 @@ connection = sqlite3.connect('{}.db'.format(bots_db))
 c = connection.cursor()
 
 def create_table():
-    c.execute("CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
+    c.execute("")
 
 if __name__ == '__main__':
-    create_table()    
+    create_table()
