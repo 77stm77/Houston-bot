b8c54dffb99d246c720d7a48196743c90732fc32
Phoenix <alexandrvirtual@gmail.com>
2019-12-31 19:13:36 +0200

    refactoring catalogs

--- houston_bot/main.py
+++ houston_bot/main.py
@@ -1,6 +1,4 @@
 import sqlite3
-import json
-import datetime
 
 bots_db = "bots_db"
 sql_transaction = []
@@ -8,8 +6,10 @@ sql_transaction = []
 connection = sqlite3.connect('{}.db'.format(bots_db))
 c = connection.cursor()
 
+
 def create_table():
     c.execute("")
+
 
 if __name__ == '__main__':
     create_table()
